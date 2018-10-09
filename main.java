cddff410c5ab97ebf7f5fa11e939137ca58015da
Iksan99 <43998935+Iksan99@users.noreply.github.com>
2018-10-09 22:46:02 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,69 @@
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("------Object Kelas Kotak------");
+		// membuat object dari class kotak
+		kotak main = new kotak();
+			
+		// method setter kotak
+		main.setpanjang(11.23);
+		main.setlebar(9.56);
+			
+		// method getter kotak
+		System.out.println("panjang : " + main.getpanjang());
+		System.out.println("lebar : " + main.getlebar());
+		System.out.println("luas : " + main.getpanjang()*main.getlebar());
+		System.out.println("keliling : " + main.getlebar()*main.getpanjang()*main.getlebar()*main.getpanjang());
+		
+		System.out.println("------Object Kelas mahasiswa------");
+		//membuat object dari class mahasiswa
+		mahasiswa mahasiswa = new mahasiswa();
+		
+		//method setter mahasiswa
+		mahasiswa.nama = "achmad ichsan";
+		mahasiswa.nim = "D0217525";
+		mahasiswa.alamat = "mapilli";
+		mahasiswa.golonganDarah = "--";
+		mahasiswa.status = "pelajar";
+		mahasiswa.tinggiBadan = "160";
+		mahasiswa.beratBadan = "50";
+		
+		//method getter mahasiswa
+		System.out.println("nama : " + mahasiswa.getnama());
+		System.out.println("nim : " + mahasiswa.getnim());
+		System.out.println("alamat : " + mahasiswa.getalamat());
+		System.out.println("golongan Darah : " + mahasiswa.getgolongnDarah());
+		System.out.println("Status : " + mahasiswa.getstatus());
+		System.out.println("tinggi badan : " + mahasiswa.gettinggiBadan());
+		System.out.println("berat badan : " + mahasiswa.getberatBadan());
+
+		System.out.println("------Object Kelas node------");
+		//membuat object dari kelas node
+		node node = new node();
+		
+		//method setter node
+		node.label = "rajin pangkal pandai";
+		node.Value = 1;
+		
+		//method getter node
+		System.out.println("Nama Label : " + node.getlabel());
+		System.out.println("jumlah label : " + node.getValue());
+
+		System.out.println("------Object Kelas stack------");
+		//membuat object dari kelas stack
+		stack stack = new stack();
+		
+		stack.value[0] = "achmad ichsan";
+		stack.value[1] = "aulia sri wahyuni umat";
+		stack.value[2]= "puput";
+		stack.value[3]= "anty";
+		stack.setvalueAt(4 ,"kaco");
+
+		//menampilkan stack
+		for(int i=0;i<stack.value.length;i++){System.out.println(stack.value[i]);};
+
+		
+		}
+	
+
+}
